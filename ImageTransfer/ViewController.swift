533a9d6e70c1a2191e3175e0366a59a807cf06a1
anthrgrnwrld <anthrgrnwrld@gmail.com>
2015-04-15 01:00:40 +0900

    test commit

--- ImageTransfer/ViewController.swift
+++ ImageTransfer/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var imageBeHereNow: UIImageView!
+    @IBOutlet var imageDestinationArea: UIImageView!
+    @IBOutlet var imageSourceArea: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        imageDestinationArea.backgroundColor = UIColor.yellowColor()
+        imageSourceArea.backgroundColor = UIColor.orangeColor()
+        imageBeHereNow.image = UIImage(named: "BeHereNow.jpg")
     }
 
     override func didReceiveMemoryWarning() {
